c71d92c7dfd73a5c3fad96b137eccb651b5bfa31
bubartem <bubartem@gmail.com>
2025-12-17 01:36:22 +0300

    HSR Update

--- src/js/games/R1999/config.js
+++ src/js/games/R1999/config.js
@@ -49,7 +49,7 @@ export const CONSTELLATION_MAP = {
 };
 
 export const UPDATE_HISTORY = ['1.0', '1.1', '1.2', '1.3', '1.4', '1.5', '1.6', '1.7', '1.8', '1.9', '2.0', '2.1', '2.2', '2.3', '2.4', '2.5', '2.6',
-    '2.7', 'AC', '2.8', '3.0'
+    '2.7', 'AC', '2.8', '3.0', '3.1'
 ]
 
 export const BANNER_HISTORY = {
